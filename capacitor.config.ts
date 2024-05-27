f271813cd5cf92fc6d1cb6a091928f7adb7a6a27
MatheusVSA <santosmatheusvicente@gmail.com>
2024-05-27 15:42:54 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'exercicio_array',
+  webDir: 'www'
+};
+
+export default config;
